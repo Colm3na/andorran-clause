f41638e1de18f19bd9d93e87a49c679664df6336
Diego Fernández Barrera <bigomby@gmail.com>
2017-08-27 18:45:00 +0200

    Fix contract wrong operation
    Use division instead of multiplication

--- contracts/contracts/AndorranClause.sol
+++ contracts/contracts/AndorranClause.sol
@@ -3,12 +3,15 @@ pragma solidity ^0.4.4;
 import "./Pact.sol";
 
 contract AndorranClause {
+	event PactCreated(address from, Pact pact);
+
 	function newPact(
 		address closer,
 		uint price,
 		address wallet,
 		address erc20
 	) returns (Pact pact) {
-		pact = new Pact(msg.sender, closer, wallet, erc20, price);
+		pact = new Pact(msg.sender, closer, price, wallet, erc20);
+		PactCreated(msg.sender, pact);
 	}
 }
--- contracts/contracts/Pact.sol
+++ contracts/contracts/Pact.sol
@@ -8,6 +8,11 @@ import "./zeppelin-solidity/token/StandardToken.sol";
 contract Pact {
 	enum State { WaitingFunds, Ready }
 
+	event Funded(uint ethers, uint tokens);
+	event Failed(uint ethers, uint tokens, uint price);
+	event Bought();
+	event Sold();
+
 	// This should be the contract AndorranClause.
 	address owner;
 
@@ -22,19 +27,19 @@ contract Pact {
 	// expected.
 	address wallet;
 
-	// Address of the Token contract for the token.
+	// Address of the Token contract to use.
 	StandardToken erc20;
 
 	// Price of the token set by "initiator" on every new pact. "closer" decides
 	// if buy or sell the tokens at this price.
-	uint price;
+	uint public price;
 
 	// Amount of ethers. The amount of tokens can be computed by multiplying
 	// "amount" and "price".
-	uint amount;
+	uint public amount;
 
 	// Current state of the contract.
-	State state;
+	State public state;
 
 	modifier whenReady {
 		require(state == State.Ready);
@@ -49,40 +54,44 @@ contract Pact {
 	function Pact(
 		address _initiator,
 		address _closer,
+		uint _price,
 		address _wallet,
-		address _erc20,
-		uint _price
+		address _erc20
 	) {
 		owner = msg.sender;
 		initiator = _initiator;
 		closer = _closer;
+		price = _price;
 		wallet = _wallet;
 		erc20 = StandardToken(_erc20);
-		price = _price;
 		state = State.WaitingFunds;
 	}
 
-	function deposit() payable {
+	function() payable {
 		require(msg.sender == wallet);
 		require(state == State.WaitingFunds);
 
 		uint tokens = erc20.balanceOf(this);
-		if (msg.value * price != tokens) {
+		if (msg.value / price != tokens) {
 			erc20.transfer(wallet, tokens);
+			Failed(msg.value, tokens, price);
 			selfdestruct(wallet);
 		}
 
 		amount = msg.value;
 		state = State.Ready;
+		Funded(msg.value, tokens);
 	}
 
 	function buy() onlyCloser whenReady {
-		erc20.transfer(closer, amount * price);
+		erc20.transfer(closer, amount / price);
+		Bought();
 		selfdestruct(wallet);
 	}
 
 	function sell() onlyCloser whenReady {
-		erc20.transfer(wallet, amount * price);
+		erc20.transfer(wallet, amount / price);
+		Sold();
 		selfdestruct(closer);
 	}
 
@@ -90,7 +99,7 @@ contract Pact {
 		require(msg.sender == initiator || msg.sender == closer);
 
 		if(state == State.Ready) {
-			erc20.transfer(wallet, amount * price);
+			erc20.transfer(wallet, amount / price);
 		}
 
 		selfdestruct(wallet);
