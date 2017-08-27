9a4b98df18e7689e162a595b1cc0a84b4ed93ed4
Diego Fernández Barrera <bigomby@gmail.com>
2017-08-27 18:47:21 +0200

    Add testnet to networks

--- contracts/truffle.js
+++ contracts/truffle.js
@@ -1,9 +1,15 @@
 module.exports = {
   networks: {
     development: {
-      host: "localhost",
+      host: 'localhost',
       port: 8545,
-      network_id: "*" // Match any network id
-    }
-  }
+      network_id: '*', // Match any network id
+    },
+    testnet: {
+      host: 'localhost',
+      port: 8545,
+      network_id: 3,
+      gas: 500000,
+    },
+  },
 };
